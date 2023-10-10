2904ee6170a5897be77fa6b338f1e0b5dd795e73
miriamjimenezj <miriam@ASUMiri>
2023-10-10 12:41:32 +0200

    carrusel funciona

---
+++ src/public/js/carousel.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elementosCarousel = document.querySelectorAll('.carousel');
+    M.Carousel.init(elementosCarousel, {
+        duration: 150,
+        dist: -120,
+        shift: 5,
+        padding: 5,
+        numVisible: 3,
+        indicators: true
+    });
+});
